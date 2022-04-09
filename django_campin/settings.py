42d387143d4d4e9d41c33e2045fe98ef2e02703b
mkang7446 <mkang7446@gmail.com>
2022-04-08 22:30:21 -0700

    set up AWS IAM

--- django_campin/settings.py
+++ django_campin/settings.py
@@ -157,5 +157,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = 'mason-campin-bucket'
+AWS_DEFAULT_ACL = 'public-read'
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+
+AWS_QUERYSTRING_AUTH = False
